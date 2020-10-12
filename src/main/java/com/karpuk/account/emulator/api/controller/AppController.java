e6e3c406157a9ac50686dcd2ec7b6bda926ed141
kateDF <kate.ff@ya.ru>
2020-10-12 11:51:18 -0400

    Add post method

--- src/main/java/com/karpuk/account/emulator/api/controller/AppController.java
+++ src/main/java/com/karpuk/account/emulator/api/controller/AppController.java
@@ -4,10 +4,7 @@ import com.karpuk.account.emulator.api.model.Account;
 import com.karpuk.account.emulator.api.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
@@ -17,9 +14,14 @@ public class AppController {
     private AccountRepository accountRepository;
 
     @GetMapping("/accounts/{id}")
-    @ResponseBody
     public Account getAccountById(@PathVariable("id") Long id) {
         return accountRepository.findAccountById(id);
     }
+
+    @PostMapping("/accounts")
+    public Account createAccount(@RequestBody Account account) {
+        return accountRepository.addAccount(account);
+    }
+
 
 }
