bd21d89dd450f10192199b42321064c8b0113108
kateDF <kate.ff@ya.ru>
2020-11-04 09:07:46 -0500

    Extract url and server port into properties files

--- src/web-layer-test/java/com/karpuk/account/emulator/test/client/TestClient.java
+++ src/web-layer-test/java/com/karpuk/account/emulator/test/client/TestClient.java
@@ -4,6 +4,7 @@ import com.karpuk.account.emulator.api.model.ApiAccount;
 import com.karpuk.account.emulator.api.model.ApiBalance;
 import com.karpuk.account.emulator.api.model.ApiTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -17,41 +18,43 @@ import java.util.List;
 @Service
 public class TestClient {
 
+    @Value("${test.base.url}")
+    private String baseUrl;
+    @Value(("${test.transactions.endpoint}"))
+    private String transactionsEndpoint;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
-    public ResponseEntity<List<ApiAccount>> getAllDbAccounts(int port) {
-        return restTemplate.exchange("http://localhost:" + port + "/accounts",
+    public ResponseEntity<List<ApiAccount>> getAllDbAccounts() {
+        return restTemplate.exchange(baseUrl,
                 HttpMethod.GET, null, new ParameterizedTypeReference<List<ApiAccount>>() {
                 });
     }
 
-    public ResponseEntity<ApiAccount> getAccountById(int port, String id) {
-        return restTemplate.getForEntity("http://localhost:" + port + "/accounts" +
-                "/" + id, ApiAccount.class);
+    public ResponseEntity<ApiAccount> getAccountById(String id) {
+        return restTemplate.getForEntity(baseUrl + "/" + id, ApiAccount.class);
     }
 
-    public ResponseEntity<ApiAccount> postAccount(int port, ApiAccount account) {
-        return restTemplate.postForEntity("http://localhost:" + port + "/accounts",
-                account, ApiAccount.class);
+    public ResponseEntity<ApiAccount> postAccount(ApiAccount account) {
+        return restTemplate.postForEntity(baseUrl, account, ApiAccount.class);
     }
 
-    public ResponseEntity<ApiBalance> addTransaction(int port, String accountId,
+    public ResponseEntity<ApiBalance> addTransaction(String accountId,
             ApiTransaction transaction) {
-        return restTemplate.postForEntity("http://localhost:" + port + "/accounts" +
-                "/" + accountId + "/transactions", transaction, ApiBalance.class);
+        return restTemplate.postForEntity(baseUrl + "/" + accountId + transactionsEndpoint, transaction, ApiBalance.class);
     }
 
-    public ResponseEntity<ApiAccount> updateAccount(int port, ApiAccount account) {
+    public ResponseEntity<ApiAccount> updateAccount(ApiAccount account) {
         RequestEntity<ApiAccount> requestEntity = RequestEntity
-                .put(URI.create("http://localhost:" + port + "/accounts"))
+                .put(URI.create(baseUrl))
                 .body(account);
         return restTemplate.exchange(requestEntity, ApiAccount.class);
     }
 
-    public ResponseEntity<ApiAccount> deleteAccount(int port, String id) {
-        RequestEntity<ApiAccount> requestEntity = new RequestEntity<>(HttpMethod.DELETE, URI.create("http://localhost" +
-                ":" + port + "/accounts" + "/" + id));
+    public ResponseEntity<ApiAccount> deleteAccount(String id) {
+        RequestEntity<ApiAccount> requestEntity = new RequestEntity<>(HttpMethod.DELETE,
+                URI.create(baseUrl + "/" + id));
         return restTemplate.exchange(requestEntity, ApiAccount.class);
     }
 
